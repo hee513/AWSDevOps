e3e0fc1289f817911ea96a2d9d400734b0d3745a
“heekyunggo” <fpdls1234@naver.com>
2021-05-12 13:15:50 +0900

    Second test file

---
+++ test2.php
@@ -0,0 +1,25 @@
+<?php
+ 
+// https://amphp.org/parallel/
+ 
+require __DIR__ . '/../vendor/autoload.php';
+ 
+use Amp\Parallel\Worker;
+use Amp\Promise;
+ 
+$urls = [
+    'https://secure.php.net',
+    'https://amphp.org',
+    'https://github.com',
+];
+ 
+$promises = [];
+foreach ($urls as $url) {
+    $promises[$url] = Worker\enqueueCallable('file_get_contents', $url);
+}
+ 
+$responses = Promise\wait(Promise\all($promises));
+ 
+foreach ($responses as $url => $response) {
+    \printf("Read %d bytes from %s\n", \strlen($response), $url);
+}
